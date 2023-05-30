a7fa8239bc8c2767e71cf4c883785580d1456fdb
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-05-30 17:50:07 +0530

    Refactor : To check who play first

--- src/com/tictactoe/TicTacToeGame.java
+++ src/com/tictactoe/TicTacToeGame.java
@@ -1,117 +1,119 @@
 package com.tictactoe;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToeGame {
-    // Created a board of char size array 10;
-    public static char[] board = new char[10];
-    public static char computerLetter;
-    public static char playerLetter;
-
-    /**
-     * This method is for creating empty board for Tic Tac Toe Game
+    /*
+     Declared instance variable
      */
-    public void createEmptyBoard() {
-        for (int index = 0; index < 10; index++) {
-            if (index == 0) {
-                continue;
-            } else {
-                board[index] = ' ';
-            }
-        }
-//        System.out.println("Currently no one is playing the game");
+    private final char[] board;
+    private char currentPlayer;
+    private char userLetter;
+    private char computerLetter;
+    private final boolean gameFinished;
+
+    /*
+     * This method is for declared the game board it's a constructor
+     */
+    public TicTacToeGame() {
+        board = new char[10]; // Index 0 is ignored
+        Arrays.fill(board, ' ');
+        currentPlayer = ' ';
+        userLetter = ' ';
+        computerLetter = ' ';
+        gameFinished = false;
     }
 
     /**
-     * This method is for taking user input for player latter and condition for the
-     * computer letter and printing
+     * This method is for choose letter X or O
      */
     public void chooseLetter() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Choose a letter :: X or O :");
-        playerLetter = sc.next().toUpperCase().charAt(0);
-        if (playerLetter == 'X') {
-            computerLetter = 'O';
-        } else {
-            computerLetter = 'X';
-        }
-        System.out.println("Player choose letter : " + playerLetter);
-        System.out.println("Computer choose letter : " + computerLetter);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Choose X or O: ");
+        userLetter = Character.toUpperCase(scanner.next().charAt(0));
+        computerLetter = (userLetter == 'X') ? 'O' : 'X';
+        System.out.println("You chose " + userLetter);
+        System.out.println("Computer is " + computerLetter);
     }
 
     /**
-     * This method is for display the current board
+     * This method is for toss who will play first
+     */
+    public void toss() {
+        Random random = new Random();
+        int result = random.nextInt(2); // 0 for heads, 1 for tails
+        currentPlayer = (result == 0) ? userLetter : computerLetter;
+        System.out.println((currentPlayer == userLetter) ? "You play first." : "Computer plays first.");
+    }
+
+    /**
+     * This method is for show the game board
      */
     public void showBoard() {
-        System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
-        System.out.println("----------");
-        System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
-        System.out.println("----------");
-        System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+        System.out.println("-------------");
+        System.out.println("| " + board[1] + " | " + board[2] + " | " + board[3] + " |");
+        System.out.println("-------------");
+        System.out.println("| " + board[4] + " | " + board[5] + " | " + board[6] + " |");
+        System.out.println("-------------");
+        System.out.println("| " + board[7] + " | " + board[8] + " | " + board[9] + " |");
+        System.out.println("-------------");
     }
 
     /**
-     * This method is for allow player to move on a desired location
+     * This method is for make the move
      */
     public void makeMove() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Choose your location(1-9): ");
-        int position = scanner.nextInt();
-        if (board[position] != ' ') {
-            System.out.println("You already chosen this! Enter a valid location");
-        } else {
-            board[position] = playerLetter;
-            showBoard();
-            checkFreeSpace();
-        }
+        int index;
+        do {
+            System.out.print("Enter the index (1-9) to make your move: ");
+            index = scanner.nextInt();
+        } while (index < 1 || index > 9 || board[index] != ' ');
+
+        board[index] = currentPlayer;
     }
 
     /**
-     * This method is for checking the free space available on board and
-     * count the number of free space and printing
+     * This method is for check available space and return true or false
      */
-
-    public void checkFreeSpace() {
-        boolean isSpaceAvailable = false;
-        int numOfFreeSpaces = 0;
-        for (int index = 1; index < board.length; index++) {
-            if ((board[index] == ' ')) {
-                isSpaceAvailable = true;
-                numOfFreeSpaces++;
+    public void isSpaceAvailable() {
+        for (int i = 1; i < board.length; i++) {
+            if (board[i] == ' ') {
+                return;
             }
         }
-        if (!isSpaceAvailable) {
-            System.out.println("Board is full! You can't make another move");
-        } else {
-            System.out.println("Free space is available! you have " + numOfFreeSpaces + " moves left");
-        }
     }
 
     /**
-     * This method is for random check the first player
+     * This method is for check winning condition
      */
+    public void checkWin(char player) {
+        // Winning combinations
+        int[][] winCombinations = {
+                {1, 2, 3}, {4, 5, 6}, {7, 8, 9}, // Rows
+                {1, 4, 7}, {2, 5, 8}, {3, 6, 9}, // Columns
+                {1, 5, 9}, {3, 5, 7}             // Diagonals
+        };
 
-    public void toss() {
-        int Head = 0;
-        double toss = Math.floor(Math.random() * 10) % 2;
-        if (toss == Head) {
-            System.out.println("computer starts to play first");
-        } else {
-            System.out.println("User starts to play first");
+        for (int[] combination : winCombinations) {
+            if (board[combination[0]] == player && board[combination[1]] == player && board[combination[2]] == player) {
+                return;
+            }
         }
     }
 
     /**
-     * Main method to execute the program and calling the method
+     * This is main method for calling the play game method
      */
     public static void main(String[] args) {
-        System.out.println("-----Welcome to tic tac toe game ------");
-        TicTacToeGame ticTacToeGame = new TicTacToeGame();
-        ticTacToeGame.createEmptyBoard();
-        ticTacToeGame.chooseLetter();
-        ticTacToeGame.showBoard();
-        ticTacToeGame.makeMove();
-        ticTacToeGame.checkFreeSpace();
-        ticTacToeGame.toss();
+        TicTacToeGame game = new TicTacToeGame();
+        game.chooseLetter();
+        game.toss();
+        game.showBoard();
+        game.makeMove();
+        game.isSpaceAvailable();
+        game.checkWin(game.currentPlayer);
     }
 }
